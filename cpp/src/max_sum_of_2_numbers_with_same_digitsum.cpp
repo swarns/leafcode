865c691e36bd442a9e1cd76d85128882497c34bd
swarn <swarnx@gmail.com>
2021-03-20 16:04:15 +0530

    max sum of 2 numbers with same digit sum

---
+++ cpp/src/max_sum_of_2_numbers_with_same_digitsum.cpp
@@ -0,0 +1,22 @@
+/*
+* Copyright 2021 @LeafCode
+*
+* Given a vector with intergers, find the maximum possible sum of two numbers whose digit sum is same.
+* if there are no possible two numbers then return -1
+*
+* eg: 
+* v = [41, 32, 54, 81, 72] then the answer would be 153 
+*
+*/
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main () {
+
+
+
+return 0;
+}
