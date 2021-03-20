d16849103822f422cd944d9dbc530ced25e595e0
swarn <swarnx@gmail.com>
2021-03-20 16:06:05 +0530

    max sum of 2 numbers with same digit sum

--- cpp/src/max_sum_of_2_numbers_with_same_digitsum.cpp
+++ cpp/src/max_sum_of_2_numbers_with_same_digitsum.cpp
@@ -1,7 +1,7 @@
 /*
 * Copyright 2021 @LeafCode
 *
-* Given a vector with intergers, find the maximum possible sum of two numbers whose digit sum is same.
+* Given a vector with intergers, find the maximum possible sum of two numbers whose digitsum is same.
 * if there are no possible two numbers then return -1
 *
 * eg: 
@@ -10,12 +10,84 @@
 */
 
 #include <iostream>
+#include <vector>
 #include <map>
 
 using namespace std;
 
+int digit_sum (int num) {
+
+	int 	dsum = 0;
+	int 	ret = 0;
+
+	while (num) {
+
+		ret = num % 10;
+		dsum += ret;
+		num = num / 10;
+	}
+
+return dsum;
+}
+
+int maxsum_of_2_numbers_with_same_digitsum (vector <int> & nums) {
+
+	int 					len = nums.size();
+	map < int, vector<int> >		m;
+	map < int, vector<int> >:: iterator 	itr;
+	int 					tmp = 0;
+	int 					max = -1;
+	
+
+	for (int i = 0; i < len; i++) {
+
+		if ( m[digit_sum(nums[i])].size() < 2 ) {
+
+			m[digit_sum(nums[i])].push_back (nums[i]);
+
+			if ( m[digit_sum(nums[i])][0] < m[digit_sum(nums[i])][1] ) {
+
+				tmp = m[digit_sum(nums[i])][0];
+				m[digit_sum(nums[i])][0] = m[digit_sum(nums[i])][1];
+				m[digit_sum(nums[i])][1] = tmp;
+			}
+
+		} else {
+
+			if ( m[digit_sum(nums[i])][0] < nums[i] ) {
+		
+				m[digit_sum(nums[i])][0] = nums[i];
+
+				if ( m[digit_sum(nums[i])][0] < m[digit_sum(nums[i])][1] ) {
+
+					tmp = m[digit_sum(nums[i])][0];
+					m[digit_sum(nums[i])][0] = m[digit_sum(nums[i])][1];
+					m[digit_sum(nums[i])][1] = tmp;
+				}
+
+			}
+
+		}
+	}
+
+	for (itr = m.begin(); itr != m.end(); itr++) {
+
+		if (itr->second.size() == 2) {
+
+			if ( max < (itr->second[0] + itr->second[1]) )
+				max = itr->second[0] + itr->second[1];
+		}
+	}
+
+return max;
+}
+
+
 int main () {
 
+	vector <int> nums = {23, 14, 90, 44, 54};
+
+	cout<<"max sum of two digits for same digitsum = "<<maxsum_of_2_numbers_with_same_digitsum (nums)<<endl;
 
 
 return 0;
